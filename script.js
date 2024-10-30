2d7a0fca31a9796eb1b8e53d37433b169daf570a
dounia-elg <dounia.elg20@gmail.com>
2024-10-30 19:20:23 +0100

    Board and Modal for add task

---
+++ script.js
@@ -0,0 +1,29 @@
+
+let currentSection = "";
+
+function openModal(section) {
+  currentSection = section;
+  document.getElementById("taskModal").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("taskModal").classList.add("hidden");
+  document.getElementById("taskTitle");
+  document.getElementById("taskDescription");
+}
+
+function addTask() {
+  const title = document.getElementById("taskTitle").value;
+  const description = document.getElementById("taskDescription").value;
+
+  const task = document.createElement("div");
+  task.classList.add("bg-white", "border", "border-gray-300", "p-3", "mb-2", "rounded", "shadow");
+
+  task.innerHTML = `
+    <h3 class="font-bold text-lg">${title}</h3>
+    <p class="text-sm text-gray-600">${description}</p>
+
+  `;
+
+  closeModal();
+}
