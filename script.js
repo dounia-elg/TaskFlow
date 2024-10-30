a7804e18f1ef72b58dc450f034b6931e7450d42e
dounia-elg <dounia.elg20@gmail.com>
2024-10-30 20:29:56 +0100

    add task deadline and change colors in model

--- script.js
+++ script.js
@@ -10,18 +10,20 @@ function closeModal() {
   document.getElementById("taskModal").classList.add("hidden");
   document.getElementById("taskTitle");
   document.getElementById("taskDescription");
+  document.getElementById("taskDeadline");
 }
 
 function addTask() {
   const title = document.getElementById("taskTitle").value;
   const description = document.getElementById("taskDescription").value;
-
+  const deadline = document.getElementById("taskDeadline").value
   const task = document.createElement("div");
   task.classList.add("bg-white", "border", "border-gray-300", "p-3", "mb-2", "rounded", "shadow");
 
   task.innerHTML = `
     <h3 class="font-bold text-lg">${title}</h3>
     <p class="text-sm text-gray-600">${description}</p>
+    <p class="text-xs text-gray-600">${deadline}</p>
 
   `;
 
