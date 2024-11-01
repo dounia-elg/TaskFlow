b08b0e2d0d22b43c5804f59c7aa819817c8afc5a
dounia-elg <dounia.elg20@gmail.com>
2024-11-01 07:27:18 +0100

    add delete button to delete task

--- script.js
+++ script.js
@@ -19,15 +19,40 @@ function addTask() {
   const description = document.getElementById("taskDescription").value;
   const deadline = document.getElementById("taskDeadline").value;
 
-  const task = document.createElement("div");
+  let task = document.createElement("div");
   task.classList.add("bg-white", "border", "border-gray-300", "p-3", "mb-2", "rounded", "shadow");
 
+
   task.innerHTML = `
-    <h3 class="font-bold text-lg">${title}</h3>
+    <h3 class="font-bold text-lg ">${title}</h3>
     <p class="text-sm text-gray-600">${description}</p>
     <p class="text-xs text-gray-600">${deadline}</p>
 
   `;
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+
+  deleteButton.classList.add("delete-btn", "bg-red-500", "text-white", "rounded" , "p2", "mt-2");
+
+  deleteButton.addEventListener("click",function(){
+    task.remove();
+  })
+
+  task.appendChild(deleteButton);
+
+  
+  if(currentSection === "To-Do"){
+    document.getElementById("todoTasks").appendChild(task);
+  }else if(currentSection === "In Progress"){
+    document.getElementById("inProgressTasks").appendChild(task);
+  }else if(currentSection === "Done"){
+    document.getElementById("doneTasks").appendChild(task);
+  }
+  
+  
+
+
 
   closeModal();
 }
