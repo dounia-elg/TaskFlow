53e3a6bccff46fa2e7cdde97694bfc7fe624a056
dounia-elg <dounia.elg20@gmail.com>
2024-11-01 10:35:29 +0100

    change the backround color of the task according to its importance

--- script.js
+++ script.js
@@ -8,20 +8,30 @@ function openModal(section) {
 
 function closeModal() {
   document.getElementById("taskModal").classList.add("hidden");
-  document.getElementById("taskTitle");
-  document.getElementById("taskDescription");
-  document.getElementById("taskDeadline");
-  document.getElementById("taskPriority");
+  document.getElementById("taskTitle").value = "";
+  document.getElementById("taskDescription").value = "";
+  document.getElementById("taskDeadline").value = "";
+  document.getElementById("taskPriority").value = "";
 }
 
 function addTask() {
   const title = document.getElementById("taskTitle").value;
   const description = document.getElementById("taskDescription").value;
   const deadline = document.getElementById("taskDeadline").value;
+  const taskPriority = document.getElementById("taskPriority").value;
+
+  console.log("Task Priority:", taskPriority);
 
   let task = document.createElement("div");
-  task.classList.add("bg-white", "border", "border-gray-300", "p-3", "mb-2", "rounded", "shadow");
+  task.classList.add( "border", "border-gray-300", "p-3", "mb-2", "rounded", "shadow");
 
+  if (taskPriority === "P1") {
+    task.classList.add("bg-red-400");
+  } else if (taskPriority === "P2") {
+    task.classList.add("bg-orange-300");
+  } else if (taskPriority === "P3") {
+    task.classList.add("bg-green-300");
+  }
 
   task.innerHTML = `
     <h3 class="font-bold text-lg ">${title}</h3>
@@ -33,7 +43,7 @@ function addTask() {
   let deleteButton = document.createElement("button");
   deleteButton.innerText = "Delete";
 
-  deleteButton.classList.add("delete-btn", "bg-red-500", "text-white", "rounded" , "p2", "mt-2");
+  deleteButton.classList.add("delete-btn", "bg-red-500", "text-white", "rounded" , "p-2", "mt-2");
 
   deleteButton.addEventListener("click",function(){
     task.remove();
@@ -42,11 +52,11 @@ function addTask() {
   task.appendChild(deleteButton);
 
   
-  if(currentSection === "To-Do"){
+    if(currentSection === "To-Do"){
     document.getElementById("todoTasks").appendChild(task);
-  }else if(currentSection === "In Progress"){
+  } else if(currentSection === "In Progress"){
     document.getElementById("inProgressTasks").appendChild(task);
-  }else if(currentSection === "Done"){
+  } else if(currentSection === "Done"){
     document.getElementById("doneTasks").appendChild(task);
   }
   
